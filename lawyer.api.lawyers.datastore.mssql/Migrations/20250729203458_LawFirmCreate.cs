7c802e80680535f16ee6bd5bcb3dc0e20d73f05c
Cesar Montalvo <1152150+cesarmrc@users.noreply.github.com>
2025-07-30 10:07:41 -0500

    Cbbwi2 codex/add lawfirm entity with properties (#7)
    * Add LawFirm entity and CRUD layers
    
    * Fix typo and add [DatabaseGenerated(DatabaseGeneratedOption.None)]
    
    * Change CityId type to GUID
    Add LawFirm mapping profile
    
    * Add migrations
    
    * update migration file

--- lawyer.api.lawyers.datastore.mssql/Migrations/20250729203458_LawFirmCreate.cs
+++ lawyer.api.lawyers.datastore.mssql/Migrations/20250729203458_LawFirmCreate.cs
@@ -11,21 +11,6 @@ namespace lawyer.api.lawyers.datastore.mssql.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "AcademicInfo",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    LawyerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AcademicInfo", x => x.Id);
-                });
 
             migrationBuilder.CreateTable(
                 name: "LawFirms",
@@ -48,29 +33,6 @@ namespace lawyer.api.lawyers.datastore.mssql.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_LawFirms", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Lawyers",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    User = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Studio = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    City = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    CredentialNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    GoogleMapsAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    SocialMediaLinks = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Lawyers", x => x.Id);
                 });
         }
 
