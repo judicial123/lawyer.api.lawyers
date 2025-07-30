bc907f0a124f9c0673e9a05a0a1905e70dbd9852
Cesar Montalvo <cesar_mrc@outlook.com>
2025-07-30 10:05:07 -0500

    Add lawyer schema

--- lawyer.api.lawyers.datastore.mssql/Model/LawyerEntity.cs
+++ lawyer.api.lawyers.datastore.mssql/Model/LawyerEntity.cs
@@ -3,7 +3,7 @@ using lawyer.api.lawyers.datastore.mssql.Model.Common;
 
 namespace lawyer.api.lawyers.datastore.mssql.Model;
 
-[Table("Lawyers")]
+[Table("Lawyers", Schema = "lawyer")]
 public class LawyerEntity : EFEntity
 {
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
